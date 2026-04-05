f300e259aff178406ee04d112b37f87a4c4dc8e9
Yann-Paul Marsch <yannpaulm@gmail.com>
2026-04-05 11:25:06 +0200

    build fail

--- frontend/src/components/wizard/WizardPage.tsx
+++ frontend/src/components/wizard/WizardPage.tsx
@@ -27,7 +27,6 @@ import {
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
-import { Input } from "@/components/ui/input";
 import { NumericInput } from "@/components/ui/numeric-input";
 import { Slider } from "@/components/ui/slider";
 import {
